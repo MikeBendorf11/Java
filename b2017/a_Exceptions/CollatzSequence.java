2b8b7aa907a3762fb7f3048db133e7a00eb7a3a9
MikeB <mikebendorf11@gmail.com>
2017-06-22 10:56:00 -0700

    collatz missing negative validation

--- b2017/a_Exceptions/CollatzSequence.java
+++ b2017/a_Exceptions/CollatzSequence.java
@@ -4,59 +4,87 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class CollatzSequence {
-	public class myExceptions extends Exception{
+	
+	/* If instance then is inner, if static then is nested.
+	 * Method call for transform(); was giving error related
+	 * with not having an instance of Collatz Sequence.
+	 * 
+	 * BigInteger Throws:NumberFormatException - val is not a valid
+	 * representation of a BigInteger in the specified radix.
+	 * */
+	public static class myExceptions extends Exception{
 		public myExceptions(String msg){ //constructor
 			super(msg);
 		}
 	}
 
 	public static void main(String[] args) {
-		/*
-		 * if odd 3n+1, if even n/2
-		 * 
-		 * BigInteger Throws:
-    NumberFormatException - val is not a valid representation 
-    of a BigInteger in the specified radix, or radix is outside 
-    the range from Character.MIN_RADIX to Character.MAX_RADIX, inclusive.
-		 */
-    	System.out.println("Character.MIN_RADIX:"+Character.MIN_RADIX);
-    	System.out.println("Character.MIN_RADIX:"+Character.MAX_RADIX);
-    	
-		String n;
+		String s;
 		BigInteger N;
 		Scanner sc = new Scanner(System.in);
 	    
 	    while(true){
 
-		    System.out.println("Enter a big integer: ");
-		    n = sc.nextLine();
+		    System.out.println("Enter as big as a positive number as you can: "
+		    		+ "(Empty line to exit)");
+		    s = sc.nextLine();
+		    if(s.equals("")) System.exit(0);
+		    
 		    try {
-		    	chucha(n);
-				toBigInt(n);
+		    	N=transform(s);
+		    	sequence(N);
+		    	System.out.println(
+		    	"The Collatz Sequence is generated from this formula: \n" +
+		    			"a) 3n + 1 if the number is odd\n" +
+		    			"b) n / 2 if the number is even\n" + 
+		    			"c) Continue until reaching 1");
 			} catch (myExceptions e) {
 				System.out.println("Error: " + e.getMessage());
 			}
-
 	    }
 	}
-	static String chucha (String a){
-		return a;	
+	
+ 	static BigInteger transform(String s) throws myExceptions{	
+		BigInteger N;
+		
+		if(testCh(s)){
+			/* Thrown is like a return
+			 * Then an immediate "else{return x;}" alternative has 
+			 * to be offered for "transform();" to be complete*/
+			throw new myExceptions("Invalid Digit Input!");
+		} else {
+			N = new BigInteger(s);
+			return N;
+		}			
+	}
+ 	
+ 	static void sequence(BigInteger N){
+ 		BigInteger N1 = new BigInteger("1");
+ 		BigInteger N2 = new BigInteger("2");
+ 		BigInteger N3 = new BigInteger("3"); 
+ 		
+ 		while (!N.equals(N1)){
+ 	 		if (N.testBit(0)){
+ 	 			N =  N1.add(N.multiply(N3));			
+ 	 		} else {
+ 	 			N = N.divide(N2);
+ 	 		}
+ 	 		System.out.println(N);
+ 		}
+ 	}
+ 	
+	static Boolean testCh(String s){	
+		Boolean b=false;
+		for(int i=0;i<s.length();i++)
+		{
+		   if(!Character.isDigit(s.charAt(i))){
+			   b=true;
+		   }
+		}
+		return b;
 	}
 	
-	static BigInteger toBigInt(String n) throws myExceptions{
-		for(int i=0;i<n.length();i++)
-		{
-		   if(!Character.isDigit(n.charAt(i))){
-			   throw new myExceptions("Error: Only numbers allowed");
-		   }
-		   
-		}
-		
-		
-
-		BigInteger N = new BigInteger(n);
-		System.out.println("Your number: " + N);
-		return N;
-	}
+	
 
 }
+
