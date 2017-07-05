97820b10a1a0c103cfce12a2e918da0dc72f07fc
MikeB <mikebendorf11@gmail.com>
2017-07-05 10:23:59 -0700

    Exercise 9.4 Console Output

--- b2017/b_StackQueueTree/TreeQueueDemo.java
+++ b2017/b_StackQueueTree/TreeQueueDemo.java
@@ -15,7 +15,30 @@ public class TreeQueueDemo {
 		myTree.printCall();
 		//Graphical Version: 
 		myTree.printCall2();
-		//Queue Non-recursive Printing
+		//Non-recursive Printing
 		myTree.printCall3();
 	}
 }
+
+/*
+Order added: 
+85 99 92 23 15 49 83 60 89 96 
+
+InOrder Recursive Printing: 
+15 23 49 60 83 85 89 92 96 99 
+
+Graphical Version: 
+                               85                               
+                ┌───────────────┴───────────────┐               
+               23                              99               
+        ┌───────┴───────┐               ┌───────┘               
+       15              49              92                       
+                        └───┐       ┌───┴───┐                   
+                           83      89      96                   
+                          ┌─┘                                   
+                         60                                     
+
+
+Non-recursive Printing using Queues: 
+85 23 99 15 49 92 83 89 96 60 
+*/
